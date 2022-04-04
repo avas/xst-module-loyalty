73bb0c9f3f6896187c01b2bdedf5e15c510e3cf2
Andrey A. Vasenev <avas.the.wise@gmail.com>
2022-04-04 17:08:17 +0700

    Make namespace style consistent across the solution.

--- src/Training.LoyaltyModule.Core/Services/IPointsOperationSearchService.cs
+++ src/Training.LoyaltyModule.Core/Services/IPointsOperationSearchService.cs
@@ -1,8 +1,9 @@
 using Training.LoyaltyModule.Core.Models;
 using VirtoCommerce.Platform.Core.GenericCrud;
 
-namespace Training.LoyaltyModule.Core.Services;
-
-public interface IPointsOperationSearchService : ISearchService<PointsOperationSearchCriteria, PointsOperationSearchResult, PointsOperation>
+namespace Training.LoyaltyModule.Core.Services
 {
+    public interface IPointsOperationSearchService : ISearchService<PointsOperationSearchCriteria, PointsOperationSearchResult, PointsOperation>
+    {
+    }
 }
--- src/Training.LoyaltyModule.Xapi/Schemas/InputRegisterPointsOperationType.cs
+++ src/Training.LoyaltyModule.Xapi/Schemas/InputRegisterPointsOperationType.cs
@@ -1,14 +1,15 @@
 using GraphQL.Types;
 
-namespace Training.LoyaltyModule.Xapi.Schemas;
-
-public class InputRegisterPointsOperationType : InputObjectGraphType
+namespace Training.LoyaltyModule.Xapi.Schemas
 {
-    public InputRegisterPointsOperationType()
+    public class InputRegisterPointsOperationType : InputObjectGraphType
     {
-        Field<NonNullGraphType<StringGraphType>>("userId", "User ID");
-        Field<StringGraphType>("storeId", "Store ID");
-        Field<NonNullGraphType<StringGraphType>>("reason", "Description of operation");
-        Field<NonNullGraphType<DecimalGraphType>>("amount", "Amount of points to deposit or grant");
+        public InputRegisterPointsOperationType()
+        {
+            Field<NonNullGraphType<StringGraphType>>("userId", "User ID");
+            Field<StringGraphType>("storeId", "Store ID");
+            Field<NonNullGraphType<StringGraphType>>("reason", "Description of operation");
+            Field<NonNullGraphType<DecimalGraphType>>("amount", "Amount of points to deposit or grant");
+        }
     }
 }
